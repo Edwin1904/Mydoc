2be61cb4763989fe0a0c822c36d3caa196050b0c
Edwin1904 <edwinjrmiller@gmail.com>
2021-03-09 20:53:02 -0600

    Finished Doctor routes

--- modules/doctor.js
+++ modules/doctor.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
+const Appointment = require('./appointment')
 
 const doctorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     }
+})
+
+doctorSchema.pre('remove', function(next) {
+    Appointment.find({ doctor: this.id }, (err, appointments) => {
+        if (err) {
+            next(err)
+        } else if (appointments.length > 0) {
+            next(new Error('This doctor has appointments still'))
+        } else {
+            next()
+        }
+    })
 })
 
 module.exports = mongoose.model('Doctor', doctorSchema)
--- routes/doctors.js
+++ routes/doctors.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Doctor = require('../modules/doctor')
-
+const Appointment = require('../modules/appointment')
 
 //All Doctors route
 router.get('/', async (req,res) => {
@@ -20,8 +20,6 @@ router.get('/', async (req,res) => {
       }
     })
 
-
-
 // New Doctor Route
 router.get('/new', (req, res) => {
     res.render('doctors/new', { doctor: new Doctor()})
@@ -35,15 +33,70 @@ router.post('/', async (req, res) => {
     })
     try {
         const newDoctor = await doctor.save()
-        res.redirect(`doctors`)
-        //res.redirect(`doctors/${newDoctor.id}`)
+        res.redirect(`doctors/${newDoctor.id}`)
       } catch {
         res.render('doctors/new', {
           doctor: doctor,
           errorMessage: 'Error creating Author'
         })
       }
-
   })
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id)
+    const appointments = await Appointment.find({ doctor: doctor.id }).limit(6).exec()
+    res.render('doctors/show', {
+      doctor: doctor,
+      doctorAppointments: appointments
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id) 
+    res.render('doctors/edit', { doctor: doctor})
+  } catch {
+    res.redirect('/doctors')
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  let doctor
+try {
+    doctor = await Doctor.findById(req.params.id)
+    doctor.name = req.body.name
+    await doctor.save()
+    res.redirect(`/doctors/${doctor.id}`)
+  } catch {
+    if (doctor == null) {
+      req.redirect('/')
+    } else {
+      res.render('doctors/edit', {
+        doctor: doctor,
+        errorMessage: 'Error updatinng Author'
+      })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  let doctor
+  try {
+      doctor = await Doctor.findById(req.params.id)
+      await doctor.remove()
+      res.redirect('/doctors')
+    } catch {
+      if (doctor == null) {
+        req.redirect('/')
+      } else {
+        res.redirect(`/doctors/${doctor.id}`)
+      }
+    }
+})
 
 module.exports = router
